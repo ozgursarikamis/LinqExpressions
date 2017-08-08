1594f756c457a1860c290515cf7db906225b5d34
ozgur.sarikamis <ozgursarikamis@gmail.com>
2017-08-08 10:47:23 +0300

    expressions in linq sample

--- LinqExpressions/Program.cs
+++ LinqExpressions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace LinqExpressions
     {
         static void Main(string[] args)
         {
+            Expression<Func<Students, bool>> isTeenagerExpr = s => s.Age > 12;
+            var isTeenager = isTeenagerExpr.Compile();
+
+            bool result = isTeenager(new Students { Age = 13 });
+            Console.WriteLine(result);
         }
+    }
+
+    class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
